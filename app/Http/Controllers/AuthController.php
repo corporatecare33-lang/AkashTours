18596df09513c98faba2934a1d7e726e9f9b5cbe
Ashik <corporatecare33@gmail.com>
2026-06-13 19:22:27 +0600

    redirect login users to dashboard

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,7 +24,7 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('/');
+            return redirect()->intended('/dashboard');
         }
 
         return back()->withErrors(['email' => 'The provided credentials do not match our records.']);
